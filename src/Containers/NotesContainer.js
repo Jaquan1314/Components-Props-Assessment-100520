c2b34ac98f499a64608e7217962417667e80e09f
Jaquan1314 <javaughngrant@gmail.com>
2020-12-08 19:52:58 -0500

    commit tree 13f8464d7f8084b11378ec30c702898958f4e3bb

--- src/Containers/NotesContainer.js
+++ src/Containers/NotesContainer.js
@@ -2,17 +2,24 @@ import React from 'react';
 import Note from '../Components/Note';
 class NotesContainer extends React.Component {
 
-    apiResponse = () => {
-        return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
-    }
+  apiResponse = () => {
+      return [{ id: 1, content: "First Note" }, { id: 2, content: "Second Note" }, { id: 3, content: "Third Note" }, { id: 4, content: "Fourth Note" }]
+  }
 
-    render() {
-        return (
-            <ul>
-                {}
-            </ul>
-        )
-    }
+  renderNotes = () => {
+    return this.apiResponse().map(note => {
+      return <Note note={note} /> 
+    })
+  }
+
+  render() {
+    console.log(this)
+    return (
+        <ul>
+          {this.renderNotes()}
+        </ul>
+    )
+  }
 
 }
 
